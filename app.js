a510f50876bb4be9685c9dd29ebcc964d3a3ffc7
Mohammad Hammad <hammad.scripted@gmail.com>
2026-06-06 15:38:36 +0530

    Refactor code structure for improved readability and maintainability

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('E-Commerce API');
+const express=require('express');
+const dotenv=require('dotenv');
+const dns=require('dns/promises');
+dns.setServers(['1.1.1.1']);
+const connectDB=require('./db/connect');
+dotenv.config();
+const app=express();
+
+
+const port=process.env.PORT || 5000;
+
+const start=async()=>{
+
+
+    try{
+
+        await connectDB(process.env.MONGO_URI);
+        console.log('Connected to MongoDB...');
+        app.listen(port,()=>{
+            console.log(`Server is listening on port ${port}...`);
+        });
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+start();
