b6ecad9dbb0b3882309e0aedc0fe8e5788723dbc
Mohammad Hammad <hammad.scripted@gmail.com>
2026-06-06 15:50:40 +0530

    Refactor app.js structure and enhance database connection settings

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express=require('express');
 const dotenv=require('dotenv');
 const dns=require('dns/promises');
+require('express-async-errors');
 dns.setServers(['1.1.1.1']);
-const connectDB=require('./db/connect');
 dotenv.config();
+const notFoundMiddleware=require('./middleware/not-found');
+const errorHandlerMiddleware=require('./middleware/error-handler');
 const app=express();
 
 
+//* database
+const connectDB=require('./db/connect');
 const port=process.env.PORT || 5000;
+
+
+//* middleware
+app.use(express.json());
+
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+})
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const start=async()=>{
 
