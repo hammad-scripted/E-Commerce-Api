20f66944e3ec650c2921050a82a1b7a4afb8860b
Mohammad Hammad <hammad.scripted@gmail.com>
2026-06-11 23:10:29 +0530

    feat: enhance review controller and routes with authentication middleware

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,41 +1,47 @@
-const Review=require('../models/Review');
-const {StatusCodes}=require('http-status-codes');
-const createReview=async(req,res)=>{
-   
-    const updatedReview=Review.findAndUpdate(req.body);
-    res.status(StatusCodes.OK).json({updatedReview});
-}
+const Review = require('../models/Review');
+const Product = require('../models/Product');
+const { NotFoundError } = require('../errors');
+const { StatusCodes } = require('http-status-codes');
+const { authorizePermissions } = require('../middleware/authentication');
+const { checkPermissions } = require('../utils/checkPermissions');
 
-const getSingleReview=async(req,res)=>{
-    const {id:reviewId}=req.params;
-   const review=await Review.findOne({_id:reviewId});
-   if(!review){
-    throw new NotFoundError(`No review with id:${reviewId}`);
-   }
-   res.status(StatusCodes.OK).json({review});
-}
-const getAllReviews=async(req,res)=>{
-    const reviews=await Review.find({});
-    res.status(StatusCodes.OK).json({reviews});
-    
-}
-const updateReview=async(req,res)=>{
-    res.send('update review')
-}
-const deleteReview=async(req,res)=>{
-   const {id:reviewId}=req.params;
+const createReview = async (req, res) => {
+  const { productId: productId } = req.params;
+  req.body.user = req.user.userId;
+  const review = await Review.create(req.body);
 
-   const review=await Review.findOne({_id:reviewId});
-if(!review){
+  res.status(StatusCodes.CREATED).json({ review });
+};
+
+const getSingleReview = async (req, res) => {
+  const { id: reviewId } = req.params;
+  const review = await Review.findOne({ _id: reviewId });
+  if (!review) {
     throw new NotFoundError(`No review with id:${reviewId}`);
-}
-await review.remove();
-res.status(StatusCodes.OK).json({"msg":"review deleted"});  
-}
-module.exports={
-    createReview,
-    getSingleReview,
-    getAllReviews,
-    updateReview,
-    deleteReview
-}       
+  }
+  res.status(StatusCodes.OK).json({ review });
+};
+const getAllReviews = async (req, res) => {
+  const reviews = await Review.find({});
+  res.status(StatusCodes.OK).json({ reviews });
+};
+const updateReview = async (req, res) => {
+  res.send('update review');
+};
+const deleteReview = async (req, res) => {
+  const { id: reviewId } = req.params;
+
+  const review = await Review.findOne({ _id: reviewId });
+  if (!review) {
+    throw new NotFoundError(`No review with id:${reviewId}`);
+  }
+  await review.remove();
+  res.status(StatusCodes.OK).json({ msg: 'review deleted' });
+};
+module.exports = {
+  createReview,
+  getSingleReview,
+  getAllReviews,
+  updateReview,
+  deleteReview,
+};
--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -1,12 +1,12 @@
 const express=require('express');
 const router=express.Router();  
-
+const {authenticateUser}=require('../middleware/authentication');
 const {createReview,getSingleReview,getAllReviews,updateReview,deleteReview}=require('../controllers/reviewController');    
 
-router.post('/',createReview);
+router.post('/',authenticateUser,createReview);
 router.get('/',getAllReviews);
 router.get('/:id',getSingleReview);
-router.patch('/:id',updateReview);
-router.delete('/:id',deleteReview);
+router.patch('/:id',authenticateUser,updateReview);
+router.delete('/:id',authenticateUser,deleteReview);
 
 module.exports=router
