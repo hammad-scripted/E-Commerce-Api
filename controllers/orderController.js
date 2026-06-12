1f2b0160af7003215b786e1bdbde145069bf542c
Mohammad Hammad <hammad.scripted@gmail.com>
2026-06-13 01:02:35 +0530

    feat: implement createOrder function with validation and payment integration

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,27 +1,93 @@
-const Order=require('../models/Order');
+const Order = require('../models/Order');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
+const { NotFoundError } = require('../errors');
+const { UnauthenticatedError } = require('../errors');
+const Product = require('../models/Product');
+const { checkPermissions } = require('../utils/checkPermissions');
 
-const createOrder=async()=>{
-    res.send('create order' );
-}
+const fakeStripeAPI = async ({ amount, currency }) => {
+  const clientSecret = 'someRandomValue';
 
-const getAllOrders=async()=>{
-    res.send('get all orders' );
-}
-const getSingleOrder=async()=>{
-    res.send("get single order");
-}
-const getCurrentUserOrders=async()=>{
-    res.send("get current user orders");
-}
-const updateOrder=async()=>{
-    res.send("update order");
-}
+  return { clientSecret, amount };
+};
+const createOrder = async (req, res) => {
+  const { items: cartItems, tax, shippingFee } = req.body;
 
+  if (!cartItems || cartItems.length < 1) {
+    throw new BadRequestError('No cart items provided');
+  }
+  if (!tax || !shippingFee) {
+    throw new BadRequestError('Please provide tax and shipping fee');
+  }
 
-module.exports={
-    createOrder,
-    getAllOrders,
-    getSingleOrder,
-    getCurrentUserOrders,
-    updateOrder,
+  //? Convert to numbers
+  const taxNum = Number(tax);
+  const shippingFeeNum = Number(shippingFee);
+
+  let orderItems = [];
+  let subtotal = 0;
+
+  for (const item of cartItems) {
+    const dbProduct = await Product.findOne({ _id: item.product });
+    if (!dbProduct) {
+      throw new NotFoundError(`No product with id:${item.product}`);
     }
+    const { name, price, image, _id } = dbProduct;
+    const amount = Number(item.amount);
+    const singleOrderItem = {
+      amount,
+      name,
+      price,
+      image,
+      product: _id,
+    };
+    orderItems = [...orderItems, singleOrderItem];
+    //? calculate subtotal
+    const itemSubtotal = amount * price;
+    subtotal += itemSubtotal;
+  }
+  //? calculate total
+  const total = subtotal + taxNum + shippingFeeNum;
+
+  // ** creating a fake clientSecret
+  const paymentIntent = await fakeStripeAPI({
+    amount: total,
+    currency: 'usd',
+  });
+
+  //? create order
+  const order = await Order.create({
+    OrderItems: orderItems,
+    tax: taxNum,
+    shippingFee: shippingFeeNum,
+    subtotal,
+    total,
+    user: req.user.userId,
+    clientSecret: paymentIntent.clientSecret,
+  });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ order, clientSecret: order.clientSecret });
+};
+
+const getAllOrders = async (req, res) => {
+  res.send('get all orders');
+};
+const getSingleOrder = async (req, res) => {
+  res.send('get single order');
+};
+const getCurrentUserOrders = async (req, res) => {
+  res.send('get current user orders');
+};
+const updateOrder = async (req, res) => {
+  res.send('update order');
+};
+
+module.exports = {
+  createOrder,
+  getAllOrders,
+  getSingleOrder,
+  getCurrentUserOrders,
+  updateOrder,
+};
--- models/Order.js
+++ models/Order.js
@@ -1,34 +1,30 @@
 const { TOO_MANY_REQUESTS } = require('http-status-codes');
-const {Schema,model}=require('mongoose');
+const { Schema, model } = require('mongoose');
 
-const singleCartItemSchema=new Schema({
-    image:{type:String,required:true},
-    name:{type:String,required:true},
-    price:{type:Number,required:true},
-    amount:{type:Number,required:true},
-    product:{type:Schema.Types.ObjectId,ref:'Product',required:true},   
-})
+const singleOrderItemSchema = new Schema({
+  image: { type: String, required: true },
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  amount: { type: Number, required: true },
+  product: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+});
 
-const orderSchema=new Schema({
-    tax:{type:Number,required:true},
-    shippingFee:{type:Number,required:true},
-    subtotal:{type:Number,required:true},
-    total:{type:Number,required:true},
-    cartItems:[
-        {
-            type:singleCartItemSchema,
-            required:true,
-        }
-    ],
-    status:{
-        type:String,
-        enum:['pending','failed','paid','delivered','cancelled'],
-        default:'pending',  
+const orderSchema = new Schema(
+  {
+    tax: { type: Number, required: true },
+    shippingFee: { type: Number, required: true },
+    subtotal: { type: Number, required: true },
+    total: { type: Number, required: true },
+    OrderItems: [singleOrderItemSchema],
+    status: {
+      type: String,
+      enum: ['pending', 'failed', 'paid', 'delivered', 'cancelled'],
+      default: 'pending',
     },
-    user:{type:Schema.Types.ObjectId,ref:'User',required:true},
-    clientSecret:{type:String,
-        required:true,
-    },
-    paymentIntentId:{type:String},
-},{timestamps:true});
-module.exports=model('Order',orderSchema);  
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    clientSecret: { type: String, required: true },
+    paymentIntentId: { type: String },
+  },
+  { timestamps: true },
+);
+module.exports = model('Order', orderSchema);
